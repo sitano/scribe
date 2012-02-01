9ca187259d90f0489a234a4c619d387fbc46a513
Goir <Goir@goirware.de>
2012-02-01 16:08:06 +0100

    Changes the way data is stored in cassandra

--- src/CassandraStorage.cpp
+++ src/CassandraStorage.cpp
@@ -7,6 +7,9 @@
 #include "conn_pool.h"
 #include <uuid/uuid.h>
 #include "CassandraStorage.h"
+#include <boost/tokenizer.hpp>
+#include <iostream>
+#include <map>
 
 using std::string;
 using boost::shared_ptr;
@@ -14,11 +17,11 @@ using namespace libcassandra;
 using namespace std;
 
 bool CassandraStorage::connectToPath(const char *uri) {
-    connected = false;    
+    connected = false;
     const char proto[] = "cassandra://";
-    
+
     if (strncmp(proto, uri, strlen(proto)) != 0) {
-        LOG_OPER("[cassandra] Invalid URI for a cassandra resource: %s", uri);      
+        LOG_OPER("[cassandra] Invalid URI for a cassandra resource: %s", uri);
     }
     uri += strlen(proto);
     const char * colon = strchr(uri, ':');
@@ -26,7 +29,7 @@ bool CassandraStorage::connectToPath(const char *uri) {
         LOG_OPER("[cassandra] Missing port specification: \"%s\"", uri);
         return false;
     }
-    
+
     char * endpnt = NULL;
     long port = strtol(colon + 1, &endpnt, 10);
     if (endpnt == colon + 1) {
@@ -43,7 +46,7 @@ bool CassandraStorage::connectToPath(const char *uri) {
         LOG_OPER("[casssandra] Invalid port specification (out of range): \"%s\"", uri);
         return false;
     }
-    string host(uri, colon - uri);    
+    string host(uri, colon - uri);
     if (*pnt == 0) {
         LOG_OPER("[casssandra] Missing keyspace name: \"%s\"", uri);
         return false;
@@ -53,11 +56,11 @@ bool CassandraStorage::connectToPath(const char *uri) {
     }
     colon = strchr(pnt, '/');
     if (!*pnt || colon == pnt || !colon || !colon[1]) {
-        LOG_OPER("[casssandra] Missing keyspace name: \"%s\"", uri);        
+        LOG_OPER("[casssandra] Missing keyspace name: \"%s\"", uri);
         return false;
     }
     string kspName_(pnt, colon - pnt);
-    
+
     pnt = colon + 1;
     while (*pnt == '/') {
         pnt++;
@@ -67,8 +70,8 @@ bool CassandraStorage::connectToPath(const char *uri) {
         LOG_OPER("[casssandra] Missing column family name: \"%s\"", uri);
         return false;
     }
-    string cfName_(pnt, colon - pnt);    
-    
+    string cfName_(pnt, colon - pnt);
+
     pnt = colon + 1;
     while (*pnt == '/') {
         pnt++;
@@ -78,32 +81,32 @@ bool CassandraStorage::connectToPath(const char *uri) {
         categoryName = new string(pnt);
         fileName = new string(*categoryName);
     } else {
-        categoryName = new string(pnt, colon - pnt);        
+        categoryName = new string(pnt, colon - pnt);
         pnt = colon + 1;
         while (*pnt == '/') {
             pnt++;
         }
         fileName = new string(pnt);
     }
-    
+
     kspName = new string(kspName_);
     cfName = new string(cfName_);
     CassandraFactory factory(host, port);
     tr1::shared_ptr<Cassandra> client_(factory.create());
     client = client_;
     connected = true;
-    LOG_OPER("Opened connection to remote Cassandra server [%s:%ld] [%s] [%s] [%s] [%s]", 
+    LOG_OPER("Opened connection to remote Cassandra server [%s:%ld] [%s] [%s] [%s] [%s]",
              host.c_str(),
              port,
              kspName->c_str(),
              cfName->c_str(),
              categoryName->c_str(),
              fileName->c_str());
-    
+
     return true;
 }
 
-CassandraStorage::CassandraStorage(const std::string& name) : 
+CassandraStorage::CassandraStorage(const std::string& name) :
   FileInterface(name, false),
   inputBuffer_(NULL),
   bufferSize_(0),
@@ -121,7 +124,7 @@ CassandraStorage::CassandraStorage(const std::string& name) :
 
 CassandraStorage::~CassandraStorage() {
     if (connected) {
-        LOG_OPER("[cassandra] disconnected Cassandra for %s", filename.c_str());        
+        LOG_OPER("[cassandra] disconnected Cassandra for %s", filename.c_str());
     }
 }
 
@@ -156,60 +159,102 @@ void CassandraStorage::close() {
     connected = false;
 }
 
-bool CassandraStorage::writeEntry(const std::string& data) {
-    if (data.length() <= 0U) {
-        return true;
+void CassandraStorage::writeEntry(std::vector<Cassandra::SuperColumnInsertTuple> *scit, const std::string& data) {
+    uuid_t uuid_c;
+    uuid_generate_time(uuid_c);
+    std::string uuid((char *) uuid_c, 16);
+    std::string cn = string("data");
+
+    std::map<std::string, std::string> values;
+    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+    boost::char_separator<char> sep("##");
+    tokenizer tokens(data, sep);
+    for (tokenizer::iterator tok_iter = tokens.begin();tok_iter != tokens.end(); ++tok_iter) {
+        //cout << *tok_iter << endl;
+        boost::char_separator<char> keyValueSep(":");
+        tokenizer keyValue(*tok_iter, keyValueSep);
+        std::vector<std::string> collection;
+        for (tokenizer::iterator keyValue_tok_iter = keyValue.begin(); keyValue_tok_iter != keyValue.end(); ++keyValue_tok_iter) {
+            collection.push_back(*keyValue_tok_iter);
+        }
+        values[collection.at(0).c_str()] = collection.at(1).c_str();
     }
-    try {
-        uuid_t uuid_c;
-        uuid_generate_time(uuid_c);
-        std::string uuid((char *) uuid_c, 16);
-        client->setKeyspace(*kspName);
-        client->insertColumn(*categoryName, *cfName, *fileName, uuid, data);
-    } catch (org::apache::cassandra::InvalidRequestException &ire) {
-        cout << ire.why << endl;
-        return false;
-    }    
-    return true;
+
+
+    if (strcmp(values["csc"].c_str(), "")== 0) {
+        cout << "MISSING CSC!" << endl;
+        return;
+    }
+    if (strcmp(values["ckey"].c_str(), "")== 0) {
+        cout << "MISSING CKEY!" << endl;
+        return;
+    }
+    cout << "added value" << endl;
+    cout << values["csc"].c_str() << " -- " << values["ckey"].c_str() << endl;
+
+
+    //<< " -- " << values["data"].c_str() << endl;
+
+    // CF, KEY, SCF, COLUM NAME, VALUE
+    //LOG_OPER("[debug] %s", values["csc"].c_str());
+    LOG_OPER("writing");
+    Cassandra::SuperColumnInsertTuple t(*categoryName, values["ckey"].c_str(), values["csc"].c_str(), cn, values["data"].c_str());
+    scit->push_back(t);
 }
+
+//csc:bla##ckey:##BYTES##
 
 bool CassandraStorage::write(const std::string& data) {
     if (!isOpen()) {
         bool success = openWrite();
-        
+
         if (!success) {
             return false;
         }
     }
-    bool ret = true;    
-    size_t start = 0U, found;    
+    bool ret = true;
+    size_t start = 0U, found;
+
+    std::vector<Cassandra::SuperColumnInsertTuple> *scit = new std::vector<Cassandra::SuperColumnInsertTuple>();
+    std::vector<Cassandra::ColumnInsertTuple> cit;
+
     do {
         found = data.find_first_of('\n', start + 1U);;
         if (found == string::npos) {
-            ret &= writeEntry(data.substr(start));
-            break;
+           writeEntry(scit, data.substr(start));
+           break;
         }
-        ret &= writeEntry(data.substr(start, found - start));        
+        writeEntry(scit, data.substr(start, found - start));
         start = found + 1U;
     } while (start < data.length());
-    
+
+    cout << "Hi" << (*scit).size() << endl;
+
+    try {
+        client->setKeyspace(*kspName);
+        client->batchInsert(cit, *scit);
+    } catch (org::apache::cassandra::InvalidRequestException &ire) {
+        cout << ire.why << endl;
+        return false;
+    }
+
     return ret;
 }
 
 void CassandraStorage::flush() {
-    
+
 }
 
 unsigned long CassandraStorage::fileSize() {
     long size = 0L;
-    
+
     if (connected) {
         org::apache::cassandra::ColumnParent col_parent;
         col_parent.column_family = *cfName;
         col_parent.super_column = *fileName;
         client->setKeyspace(*kspName);
-        size = 0;
-	//size = ksp->getCount(*categoryName, col_parent);
+        size = 1024;
+        //client->getCount(*categoryName, col_parent);
     }
     return size;
 }
@@ -228,7 +273,7 @@ void CassandraStorage::listImpl(const std::string& path,
 }
 
 long CassandraStorage::readNext(std::string& _return) {
-    return 0;           // frames not yet supported
+    return false;           // frames not yet supported
 }
 
 string CassandraStorage::getFrame(unsigned data_length) {
@@ -245,9 +290,8 @@ bool CassandraStorage::createDirectory(std::string path) {
  * normal file and write the symlink data into it
  */
 bool CassandraStorage::createSymlink(std::string oldpath, std::string newpath) {
-    bool ret = writeEntry(oldpath);
-    
-    return ret;
+    return false;
 }
 
 #endif
+
