9fa7c3eb9114819ff09b34035fbbda73e1b674b3
Goir <goir@goirware.de>
2012-02-06 09:15:04 -0500

    changed log message on cassandra write

--- src/CassandraStore.cpp
+++ src/CassandraStore.cpp
@@ -37,7 +37,6 @@ void CassandraStore::configure(pStoreConf configuration, pStoreConf parent) {
     Store::configure(configuration, parent);
     // Error checking is done on open()
     if (!configuration->getString("remote_host", remoteHost))  {
-        throw runtime_error("Bad Config - remote_host not set");
         LOG_OPER("[%s] Bad Config - remote_host not set", categoryHandled.c_str());
     }
 
@@ -135,6 +134,7 @@ bool CassandraStore::handleMessages(boost::shared_ptr<logentry_vector_t> message
     vector<Cassandra::ColumnInsertTuple> *cit = new std::vector<
             Cassandra::ColumnInsertTuple>();
 
+    unsigned int rows = 0;
     for (logentry_vector_t::iterator iter = messages->begin();
            iter != messages->end();
            ++iter) {
@@ -166,6 +166,9 @@ bool CassandraStore::handleMessages(boost::shared_ptr<logentry_vector_t> message
         if (!parseJsonMessage(message, rowKey, scName, scit, cit)) {
             LOG_OPER("could not create insert touple for <%s>", message.c_str());
         }
+        else {
+        	rows++;
+        }
     }
 
     if (scit->size() > 0 || cit->size() > 0) {
@@ -175,9 +178,8 @@ bool CassandraStore::handleMessages(boost::shared_ptr<logentry_vector_t> message
             unsigned long start = scribe::clock::nowInMsec();
             client->batchInsert(*cit, *scit, org::apache::cassandra::ConsistencyLevel::ONE);
             unsigned long runtime = scribe::clock::nowInMsec() - start;
-            LOG_OPER("[%s] [Cassandra] [%s] wrote %lu super columns and %lu columns in <%lu>",
-                    categoryHandled.c_str(), client->getHost().c_str(), scit->size(),
-                    cit->size(), runtime);
+            LOG_OPER("[%s] [Cassandra] [%s] wrote <%i> rows in <%lu>",
+                    categoryHandled.c_str(), client->getHost().c_str(), rows, runtime);
         } catch (org::apache::cassandra::InvalidRequestException &ire) {
             cout << ire.why << endl;
             success = false;
